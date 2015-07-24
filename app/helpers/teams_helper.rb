1adeaed27a3fd678a87047d89ffecfb145bc786e
Victor Pereira <vpereira@suse.de>
2015-07-24 11:10:20 +0200

    styling the team

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,6 @@
 module TeamsHelper
+  def print_status(task)
+    str, css_classes = task.finished? ? ["Done","label label-succes"] : ["Running","label label-info"]
+    content_tag :span, str, class: css_classes
+  end
 end
